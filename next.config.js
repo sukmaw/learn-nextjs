1dbe1df03f7e3d14f9eed1c5523e0fae7da26b75
Luthfi <luthfi552@gmail.com>
2024-06-24 16:09:42 +0700

    add: exoeriment actions

--- next.config.js
+++ next.config.js
@@ -9,6 +9,12 @@ const nextConfig = {
       },
     ];
   },
+  experimental: {
+    serverActions: {
+      allowedForwardedHosts: ['localhost'],
+      allowedOrigins: ['http://localhost'],
+    },
+  },
 };
 
 module.exports = nextConfig;
